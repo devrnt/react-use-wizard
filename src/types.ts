8fdb6b53d415910d9714cc4f571f5c7b6ff958de
devrnt <devrient.jonas@gmail.com>
2020-12-30 16:23:46 +0100

    chore: add types

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export type Handler = (() => Promise<void>) | (() => void) | null;
+
+export type WizardProps = {
+  footer?: React.ReactNode;
+};
+
+export type WizardValues = {
+  nextStep: () => Promise<void>;
+  previousStep: () => void;
+  handleStep: (handler: Handler) => void;
+  isLoading: boolean;
+  activeStep: number;
+  isFirstStep: boolean;
+  isLastStep: boolean;
+} | null;
