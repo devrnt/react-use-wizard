3d0906048ca4370b5beeb799eb67f3e2b36bad6a
devrnt <devrient.jonas@gmail.com>
2020-12-31 12:48:29 +0100

    test: add wizard tests

---
+++ test/wizard.test.tsx
@@ -0,0 +1,51 @@
+import { render } from '@testing-library/react';
+import * as React from 'react';
+
+import { Wizard } from '../src';
+
+const Component = (
+  <Wizard>
+    <p>step 1</p>
+    <p>step 2</p>
+  </Wizard>
+);
+
+const ComponentWithHeader = (
+  <Wizard header={<p>header</p>}>
+    <p>step 1</p>
+    <p>step 2</p>
+  </Wizard>
+);
+
+const ComponentWithFooter = (
+  <Wizard header={<p>header</p>}>
+    <p>step 1</p>
+    <p>step 2</p>
+  </Wizard>
+);
+
+describe('Wizard', () => {
+  test('should render first step', () => {
+    const { queryByText } = render(Component);
+
+    expect(queryByText('step 1')).toBeInTheDocument();
+  });
+
+  test('should not render second step', () => {
+    const { queryByText } = render(Component);
+
+    expect(queryByText('step 2')).not.toBeInTheDocument();
+  });
+
+  test('should render header', () => {
+    const { queryByText } = render(ComponentWithHeader);
+
+    expect(queryByText('header')).toBeInTheDocument();
+  });
+
+  test('should render footer', () => {
+    const { queryByText } = render(ComponentWithFooter);
+
+    expect(queryByText('header')).toBeInTheDocument();
+  });
+});
