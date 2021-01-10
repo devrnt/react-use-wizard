f73f4e91e2399d821454353824dcc171083b5ec3
devrnt <devrient.jonas@gmail.com>
2021-01-10 22:48:01 +0100

    chore: use promise instead of async await

--- src/wizard.tsx
+++ src/wizard.tsx
@@ -35,16 +35,17 @@ const Wizard: React.FC<WizardProps> = React.memo(
 
     const doNextStep = React.useCallback(async () => {
       if (hasNextStep.current && nextStepHandler.current) {
-        try {
-          setIsLoading(true);
-          await nextStepHandler.current();
-          setIsLoading(false);
-          nextStepHandler.current = null;
-          goToNextStep();
-        } catch (error) {
-          setIsLoading(false);
-          throw error;
-        }
+        setIsLoading(true);
+        Promise.resolve(nextStepHandler.current)
+          .then(() => {
+            setIsLoading(false);
+            nextStepHandler.current = null;
+            return goToNextStep();
+          })
+          .catch((error) => {
+            setIsLoading(false);
+            throw error;
+          });
       } else {
         goToNextStep();
       }
