76c8b4e0b6423a195971fa479af1d27aeb19a0df
Jonas De Vrient <32844711+devrnt@users.noreply.github.com>
2022-06-30 20:20:23 +0000

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-export { default as Wizard } from './wizard';
-export { default as useWizard } from './useWizard';
+import { default as Wizard } from './wizard';
+import { default as useWizard } from './useWizard';
+
+// Type re-export workaround, to stay compatible with TS 3.7 and lower
+import {
+  WizardProps as _WizardProps,
+  WizardValues as _WizardValues,
+  Handler as _Handler,
+} from './types';
+
+export type WizardProps = _WizardProps;
+export type WizardValues = _WizardValues;
+export type Handler = _Handler;
+
+export { Wizard, useWizard };
